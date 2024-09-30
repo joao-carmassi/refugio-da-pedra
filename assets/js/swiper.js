c796bf9aecdd5eaec0f3d67ac50f84bb5f1a8173
Joao Carmassi <joaovitorcarmassi@gmail.com>
2024-09-30 01:48:12 -0300

    criacao do card

---
+++ assets/js/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  speed: 600,
+  slidesPerView: 3,
+  autoplay: {
+    delay: 5000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
