d801171bbe324345f3dfa9f960404dd85b14d402
Joao Carmassi <joaovitorcarmassi@gmail.com>
2024-09-30 16:21:51 -0300

    card e galeria de fotos

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,16 +1,27 @@
+// Dooms -----
+const inputCardNext = document.querySelector(".swiper-button-next");
+
+// Vars -----
+let x = 0;
+
+// Functions -----
+scrowCard();
+
+// Code -----
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
   speed: 600,
   slidesPerView: 3,
-  autoplay: {
-    delay: 5000,
-  },
+  //   autoplay: {
+  //     delay: 5000,
+  //   },
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: "true",
   },
 
   // Navigation arrows
@@ -19,3 +30,19 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+function scrowCard() {
+  window.addEventListener("scroll", () => {
+    if (x == 0) {
+      const intevalorSwiper1 = setInterval(() => {
+        inputCardNext.click();
+        clearInterval(intevalorSwiper1);
+      }, 2500);
+      const intevalorSwiper2 = setInterval(() => {
+        inputCardNext.click();
+        clearInterval(intevalorSwiper2);
+      }, 5000);
+      x++;
+    }
+  });
+}
